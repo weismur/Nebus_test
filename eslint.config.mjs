f86f9577bd890b851ad019d131d5b2658d9029eb
Хохлова Ирина Игоревна <khokhlova-iri@ranepa.ru>
2026-08-19 13:16:39 +0300

    Initial commit: Nuxt 4 app with Pinia, SCSS, ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+  typescript: true,
+  formatters: {
+    css: true,
+    html: true,
+    markdown: true,
+  },
+})
